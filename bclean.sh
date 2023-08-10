46eb28919058ba7809586072e0c951c1597421a3
Scott Lloyd <scott_lloyd@byu.edu>
2023-08-09 21:47:54 -0600

    add file check to BOOT.bin target

--- bclean.sh
+++ bclean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # clean build directory
 # run from build directory: ../bclean.sh
-rm -rf CMake* cmake* Makefile drivers lab* archive instructor
+rm -rf CMake* cmake* Makefile drivers lab* archive instructor BOOT.bin conf.bif
